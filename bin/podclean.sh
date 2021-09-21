442bb4311b7b2e744eb7b34af1cb0d2e7203b994
Marcos Curvello <mrcurvello@gmail.com>
2021-09-21 15:28:15 -0300

    Add clean cocoapods script

---
+++ bin/podclean.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+############
+### zsh script to clear cocoapods caches
+###
+############
+
+function cleanCocoapods() {
+    rm -rf "${HOME}/Library/Caches/CocoaPods\ Pods"
+    rm -rf "${HOME}/Library/Developer/Xcode/DerivedData"
+    rm -rf "${HOME}/Library/Caches/CocoaPods"
+    pod cache clean --all
+    pod update
+    pod deintegrate
+    pod install
+}
+
+# ensures that you are inside a Xcode 
+function ensureCocoapods() {
+	local cocoapods return variable=$1
+
+	cocoapods=(Pods/)
+
+	if [ -e "${cocoapods}" ]; then
+		return=true
+	else
+		return=false
+	fi
+
+	eval $variable="'$return'"
+}
+
+# script main entry point
+unsetopt nomatch
+
+ensureCocoapods contains_pod_folder
+
+if $contains_pod_folder; then
+	echo "✓ Cleaning Cocoapods..."
+	cleanCocoapods
+else 
+    echo "⨯ No Pods folder found"
+fi
+
