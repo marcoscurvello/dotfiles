65bbb95fab9c817dd3ade880e0b3256dbf548fc8
Marcos Curvello <mrcurvello@gmail.com>
2026-09-15 13:22:55 +0100

    refactor(zsh): simplify utility helpers

--- home/.zsh/functions/xcode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOTFILES_DIR="$HOME/.dotfiles/home"
-XCODE_USER_DATA="$HOME/Library/Developer/Xcode/UserData"
-
-mkdir -p "$XCODE_USER_DATA"
-
-ln -sf "$DOTFILES_DIR/Library/Developer/Xcode/UserData/FontAndColorThemes" "$XCODE_USER_DATA/FontAndColorThemes"
